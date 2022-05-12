6e1c0356c3138cfbdf9db9fd78030dfee5e3f087
shahshyam <shyam.sundar525@gmail.com>
2022-05-12 15:06:30 +0545

    added log message

--- js/command.js
+++ js/command.js
@@ -24,6 +24,7 @@ function setLocation(locationValue) {
   );
 }
 function addTextOndescription() {
+  console.log("function is calling");
   let item = Office.context.mailbox.item;
   let body =
     "<p><br/><br/>___________________________________________________________________ <br>" +
@@ -31,48 +32,49 @@ function addTextOndescription() {
     "<br/>At the given moment, just put in a pair of airbuds or headphones and, from your phone.<br/>" +
     "Press the link and follow the flow, you'll automatically be taken to your feeting. " +
     "<br/><br/>No worries if you're not able to walk, you can join from your desktop as well.<br/>" +
-    "Feeting is the #1 walking meeting platform 🚶👣 🤙 </p>";
-  item.body.getTypeAsync(function (result) {
-    if (result.status == Office.AsyncResultStatus.Succeeded) {
-      if (result.value == Office.MailboxEnums.BodyType.Html) {
-        item.body.setAsync(
-          body,
-          {
-            coercionType: Office.CoercionType.Html,
-            asyncContext: { var3: 1, var4: 2 },
-          },
-          function (asyncResult) {
-            if (asyncResult.status == Office.AsyncResultStatus.Failed) {
-              statusUpdate(
-                "informationalMessage",
-                "Location added successfully"
-              );
-            } else {
-              statusUpdate("errorMessage", "Unable to insert location");
-            }
-          }
-        );
-      } else {
-        item.body.setAsync(
-          body,
-          {
-            coercionType: Office.CoercionType.Text,
-            asyncContext: { var3: 1, var4: 2 },
-          },
-          function (asyncResult) {
-            if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
-              statusUpdate(
-                "informationalMessage",
-                "Location added successfully"
-              );
-            } else {
-              statusUpdate("errorMessage", "Unable to insert location");
-            }
-          }
-        );
-      }
-    }
-  });
+    "Feeting is the #1 walking meeting platform 🚶👣 🤙 </p>';
+     item.body.setAsync(
+       body,
+       {
+         coercionType: Office.CoercionType.Html,
+         asyncContext: { var3: 1, var4: 2 },
+       },
+       function (asyncResult) {
+         if (asyncResult.status == Office.AsyncResultStatus.Failed) {
+           console.log("before updating status failed");
+           statusUpdate("errorMessage", "failed added description");
+           console.log("function is calling");
+         } else {
+           console.log("before updating status sucess");
+           statusUpdate("informationalMessage", "Successfull added description");
+           console.log("function is calling");
+         }
+       }
+     );
+  // item.body.getTypeAsync(function (result) {
+  //   if (result.status == Office.AsyncResultStatus.Succeeded) {
+  //     if (result.value == Office.MailboxEnums.BodyType.Html) {
+  //     } else {
+  //       item.body.setAsync(
+  //         body,
+  //         {
+  //           coercionType: Office.CoercionType.Text,
+  //           asyncContext: { var3: 1, var4: 2 },
+  //         },
+  //         function (asyncResult) {
+  //           if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
+  //             statusUpdate(
+  //               "informationalMessage",
+  //               "Location added successfully"
+  //             );
+  //           } else {
+  //             statusUpdate("errorMessage", "Unable to insert location");
+  //           }
+  //         }
+  //       );
+  //     }
+  //   }
+  // });
 }
 // Helper function to add a status message to the info bar.
 function statusUpdate(actionType, text) {
