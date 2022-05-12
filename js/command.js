d4e6f5380ed182b4dee066fb1574167576e7180e
shahshyam <shyam.sundar525@gmail.com>
2022-05-12 15:06:59 +0545

    fixed minor issue

--- js/command.js
+++ js/command.js
@@ -32,25 +32,25 @@ function addTextOndescription() {
     "<br/>At the given moment, just put in a pair of airbuds or headphones and, from your phone.<br/>" +
     "Press the link and follow the flow, you'll automatically be taken to your feeting. " +
     "<br/><br/>No worries if you're not able to walk, you can join from your desktop as well.<br/>" +
-    "Feeting is the #1 walking meeting platform 🚶👣 🤙 </p>';
-     item.body.setAsync(
-       body,
-       {
-         coercionType: Office.CoercionType.Html,
-         asyncContext: { var3: 1, var4: 2 },
-       },
-       function (asyncResult) {
-         if (asyncResult.status == Office.AsyncResultStatus.Failed) {
-           console.log("before updating status failed");
-           statusUpdate("errorMessage", "failed added description");
-           console.log("function is calling");
-         } else {
-           console.log("before updating status sucess");
-           statusUpdate("informationalMessage", "Successfull added description");
-           console.log("function is calling");
-         }
-       }
-     );
+    "Feeting is the #1 walking meeting platform 🚶👣 🤙 </p>";
+  item.body.setAsync(
+    body,
+    {
+      coercionType: Office.CoercionType.Html,
+      asyncContext: { var3: 1, var4: 2 },
+    },
+    function (asyncResult) {
+      if (asyncResult.status == Office.AsyncResultStatus.Failed) {
+        console.log("before updating status failed");
+        statusUpdate("errorMessage", "failed added description");
+        console.log("function is calling");
+      } else {
+        console.log("before updating status sucess");
+        statusUpdate("informationalMessage", "Successfull added description");
+        console.log("function is calling");
+      }
+    }
+  );
   // item.body.getTypeAsync(function (result) {
   //   if (result.status == Office.AsyncResultStatus.Succeeded) {
   //     if (result.value == Office.MailboxEnums.BodyType.Html) {
